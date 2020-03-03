0a9d9581e4f950c1354d6d3eab532f497c613788
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-03 17:10:54 +0100

    updated the Money class

--- src/banking/Money.cpp
+++ src/banking/Money.cpp
@@ -1,7 +1,42 @@
-#ifndef MONEY_H
-#define MONEY_H
+#include "Money.h"
+
+banking::Money::Money(int a_dollars): Money(a_dollars,0)
+{
 
 
+}
 
+banking::Money::Money(int a_dollars, int a_cents)
+{
+    this->m_cents = (100 * a_dollars) + a_cents;
+}
 
-#endif // MONEY_H
+banking::Money::Money(const Money & a_toCopy)
+{
+    this->m_cents = a_toCopy.m_cents;
+}
+
+banking::Money::~Money()
+{
+
+}
+
+void banking::Money::add(Money a_amountToAdd)
+{
+    this->m_cents += a_amountToAdd.m_cents;
+}
+
+void banking::Money::subtract(Money a_amountToSubtract)
+{
+    this->m_cents -= a_amountToSubtract.m_cents;
+}
+
+bool banking::Money::lessEqual(Money a_compareTo)
+{
+    return this->m_cents <= a_compareTo.m_cents;
+}
+
+QString banking::Money::toString()
+{
+ return "$" + QString::number(m_cents/100) + (m_cents %100 >= 10  ? "." + QString::number(m_cents % 100) : ".0" + QString::number(m_cents % 100));
+}
--- src/banking/Money.h
+++ src/banking/Money.h
@@ -1,7 +1,75 @@
+/**
+ * @file Money.h
+ * @brief Header file of the Money class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
 #ifndef MONEY_H
 #define MONEY_H
+#include <QString>
+/**
+ * @brief Representation for money amounts
+ */
+namespace banking
+{
+class Money
+{
+
+public:
+    /** Constructor
+     *
+     *  @param dollars whole dollar amount
+     */
+    explicit Money(int a_dollars);
+    /** Constructor
+     *
+     *  @param dollars dollar part of amount
+     *  @param cents cents part of amount
+     */
+    explicit Money(int a_dollars, int a_cents);
+    /** Copy constructor
+     *
+     *  @param toCopy the Money object to copy
+     */
+    Money(const Money & a_toCopy);
+    /** Destructor
+     */
+    ~Money();
+    /** Create a string representation of this amount
+     *
+     *  @return string representation of this amount
+     */
+    QString toString();
+    /** Add an amount of money to this
+     *
+     *  @param amountToAdd the amount to add
+     */
+    void add(Money a_amountToAdd);
+    /** Subtract an amount of money from this
+     *
+     *  @param amountToSubtract the amount to subtract
+     *
+     *  Precondition: amount must be <= this amount
+     */
+    void subtract(Money a_amountToSubtract);
+    /** Compare this to another amount
+     *
+     *  @param compareTo the amount to compare to
+     *  @return true if this amount is <= compareTo amount
+     */
+    bool lessEqual(Money a_compareTo);
 
 
+protected:
 
+
+private:
+    /** Instance variable: this amount represented as a number of cents
+      */
+    int m_cents;
+
+};
+}
 
 #endif // MONEY_H
