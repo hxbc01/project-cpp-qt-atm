05665e31772e9782e9a0d3c1c655c8d837c2aa2d
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-03 20:01:47 +0100

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 
 #include <QApplication>
-#include <QString>
-//#include "mainwindow.h"
+#include <iostream>
+#include <stdio.h>
+#include "Money.h"
+using namespace std;
+using namespace banking;
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-//MainWindow mainWindow;
-    //mainWindow.show();
-    return app.exec();
+    QString text = "test";
+    Money money1(100,12),money2(12,1);
+    //money1.add(money2);
+    cout << money1.toString().toStdString() <<endl;
+    return 0;
 }
