0dce352cfaeaf0f7d417a42dde056e0143fce3d2
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-04 17:40:11 +0100

    updated Card  class

--- src/banking/Card.cpp
+++ src/banking/Card.cpp
@@ -1,7 +1,24 @@
-#ifndef CARD_H
-#define CARD_H
+#include <QDebug>
+#include "Card.h"
+
+banking::Card::Card()
+{
+
+}
+banking::Card::Card(int a_number):m_number(a_number)
+{
+
+}
+
+banking::Card::~Card()
+{
+
+}
+
+int banking::Card::getNumber()
+{
+    return m_number;
+}
 
 
 
-
-#endif // CARD_H
--- src/banking/Card.h
+++ src/banking/Card.h
@@ -1,7 +1,48 @@
+/**
+ * @file Card.h
+ * @brief Header file of the Card class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
 #ifndef CARD_H
 #define CARD_H
+/** Representation for customer's ATM card
+ */
+namespace banking
+{
+class Card
+{
+
+public:
+    /** Default Constructor
+     */
+    Card();
+    /** Constructor
+     *
+     *  @param number the card number
+     */
+    explicit Card(int a_number);
+
+    /** Destructor
+     */
+    ~Card();
+    /** Accessor for number
+      *
+      *  @return the number of the card
+      */
+    int getNumber();
+
+protected:
 
 
+private:
+    /** Card number encoded on the card
+     */
+    int m_number;
 
+};
+}
 
 #endif // CARD_H
+
