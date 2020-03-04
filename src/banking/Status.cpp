a09f9dfbaa56aeda3c64e34062084b17d9d0d01f
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-04 17:41:54 +0100

    updated Status class

--- src/banking/Status.cpp
+++ src/banking/Status.cpp
@@ -1,4 +1,28 @@
+#include <QDebug>
 #include "Status.h"
+
+banking::Status::Status()
+{
+
+}
+
+banking::Status::~Status()
+{
+
+}
+
+QString banking::Status::toString()
+{
+    if (isSuccess())
+        return "SUCCESS";
+    else if (isInvalidPIN())
+        return "INVALID PIN";
+    else
+        return "FAILURE " + getMessage();
+}
+
+
+
 
 
 
--- src/banking/Status.h
+++ src/banking/Status.h
@@ -7,12 +7,54 @@
  */
 #ifndef STATUS_H
 #define STATUS_H
-
-/**
- * @brief The Status class
+#include <QString>
+/** Abstract base class for representation of various status codes returned
+ *  by bank for a transaction.  The bank will create appropriate subclasses.
  */
+namespace banking
+{
+class Status
+{
+
+public:
+    /** Default Constructor
+     */
+    Status();
+    /** Destructor
+     */
+    virtual ~Status();
+    /** See if this status represents success
+     *
+     *  @return true if this status represents success
+     */
+    virtual bool isSuccess() = 0;
+
+    /** See if this status represents an invalid PIN
+     *
+     *  @return true if this status represents an invalid PIN
+     */
+    virtual bool isInvalidPIN() = 0;
+
+    /** Accessor for message describing this status (used if status is
+     *  not success)
+     *
+     *  @return description of the problem
+     */
+    virtual QString getMessage() = 0;
+    /** Create a printable string representing this status
+     *
+     *  @return string representation
+     */
+    QString toString();
+
+protected:
 
 
+private:
 
+
+};
+}
 
 #endif // STATUS_H
+
