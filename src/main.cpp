cd0c946f6d48a0c19e79a025ccb8acccd930f539
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-04 17:42:26 +0100

    changed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <stdio.h>
 #include "Money.h"
+#include "Card.h"
 using namespace std;
 using namespace banking;
 
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     QString text = "test";
     Money money1(100,12),money2(12,1);
-    //money1.add(money2);
-    cout << money1.toString().toStdString() <<endl;
+    Card card1(100);
+    cout << card1.getNumber() <<endl;
     return 0;
 }
