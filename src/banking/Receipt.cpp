da3218c797c0cbc35a1de8ac9adac949efe216b9
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-06 20:42:53 +0100

    update Receipt files

--- src/banking/Receipt.cpp
+++ src/banking/Receipt.cpp
@@ -1,7 +1,35 @@
-#ifndef RECEIPT_H
-#define RECEIPT_H
+#include <QDebug>
+#include "Receipt.h"
+
+banking::Receipt::Receipt()
+{
+
+}
+banking::Receipt::Receipt(ATM a_atm, Card a_card, Transaction a_transaction, Balances a_balances , QDateTime a_currentDate)
+{
+    // Heading portion of the receipt
+
+    m_headingPortion.append(a_currentDate.currentDateTime().toString());
+    m_headingPortion.append(a_atm.getBankName());
+    m_headingPortion.append("ATM #" + a_atm.getID() + " " + a_atm.getPlace());
+    m_headingPortion.append("CARD " + a_card.getNumber() +
+                            " TRANS #" + a_transaction.getSerialNumber());
+
+    // The constructor for each subclass will fill in the details array
+    // appropriately
+
+    // Balances portion of the receipt
+
+    m_balancesPortion.append("TOTAL BAL: " + a_balances.getTotal().toString());
+    m_balancesPortion.append("AVAILABLE: " + a_balances.getAvailable().toString());
+
+}
+
+banking::Receipt::~Receipt()
+{
+
+}
 
 
 
 
-#endif // RECEIPT_H
--- src/banking/Receipt.h
+++ src/banking/Receipt.h
@@ -1,7 +1,64 @@
+/**
+ * @file Receipt.h
+ * @brief Header file of the Receipt class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
 #ifndef RECEIPT_H
 #define RECEIPT_H
+#include <QStringList>
+#include <QDateTime>
+#include "Balances.h"
+#include "Transaction.h"
+#include "Card.h"
+#include "ATM.h"
+/** Abstract base class for representation of a receipt to be printed.  Each
+ *  specific type of transaction creates an instance of a concrete subclass
+ *  of this class.
+ */
+
+namespace banking
+{
+class Receipt
+{
+    /** Default Constructor
+     */
+    Receipt();
+    /** Constructor.  This base class constructor will create the portions of the
+     *  receipt that are common to all types of transaction, and the subclass
+     *  constructor will create the details portion unique to each type.
+     *
+     *  @param atm the ATM where the transaction was done
+     *  @param card the card that was used
+     *  @param transaction the Transaction object for the transaction
+     *  @param balances Balances object giving final balances for account used
+     */
+    Receipt(ATM a_atm, Card a_card, Transaction a_transaction, Balances a_balances, QDateTime a_currentDate);
+    /** Destructor
+     */
+    ~Receipt();
+public:
 
 
+protected:
 
+    /** Transaction details portion of the receipt - specific to each type of
+     *  transaction, and therefore filled in by subclasses
+     */
+    QStringList m_detailsPortion;
+
+private:
+
+    /** Heading portion of the receipt - common to all forms of receipt
+     */
+    QStringList m_headingPortion;
+    /** Ending balances portion of the receipt - common to all forms of receipt
+     */
+    QStringList m_balancesPortion;
+};
+}
 
 #endif // RECEIPT_H
+
+
