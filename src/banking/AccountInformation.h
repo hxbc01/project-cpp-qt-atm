89b4bcad3cd5bd1d351d5d30391f6b012edcc88f
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-06 20:39:57 +0100

    update AccountInformation files

--- src/banking/AccountInformation.h
+++ src/banking/AccountInformation.h
@@ -7,7 +7,7 @@
  */
 #ifndef ACCOUNTINFORMATION_H
 #define ACCOUNTINFORMATION_H
-#include <QString>
+#include <QStringList>
 /** The static members of this class incorporate information about accounts
  *  offered by the bank.  Actual communication with the bank uses account type
  *  numbers - e.g. 0 represents the first type of account listed.
@@ -26,10 +26,10 @@ public:
     ~AccountInformation();
     /** Names for accounts to be offered in menu to customer
      */
-    QString ACCOUNT_NAMES[3] = {"Checking", "Savings", "Money Market"};
+    const QStringList ACCOUNT_NAMES = {"Checking", "Savings", "Money Market"};
     /** Abbreviations for account names to be printed on receipts.
       */
-    QString ACCOUNT_ABBREVIATIONS[3] = {"CHKG", "SVGS", "MMKT"};
+    const QStringList ACCOUNT_ABBREVIATIONS = {"CHKG", "SVGS", "MMKT"};
 
 protected:
 
