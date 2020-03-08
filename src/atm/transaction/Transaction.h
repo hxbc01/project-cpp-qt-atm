a32dab9c1ec264a0d1c2656ee3114029ed464ef3
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-08 18:11:42 +0100

    Update Transaction files

--- src/atm/transaction/Transaction.h
+++ src/atm/transaction/Transaction.h
@@ -1,7 +1,192 @@
+/**
+ * @file Transaction.h
+ * @brief Header file of the Transaction class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
+/** Abstract base class for classes representing the various kinds of
+ *  transaction the ATM can perform
+ */
+class ATM;
+class Session;
+class Card;
+
+namespace atm
+{
+namespace transaction
+{
+class Transaction
+{
+
+public:
+    /** Default Constructor
+     */
+    Transaction();
+    /** Constructor
+     *
+     *  @param atm the ATM used to communicate with customer
+     *  @param session the session in which this transaction is being performed
+     *  @param card the customer's card
+     *  @param pin the PIN entered by the customer
+     */
+    Transaction(ATM *ap_atm, Session *ap_session, Card *ap_card, int a_pin);
+
+    /** Destructor
+     */
+    ~Transaction();
+    /** Create a transaction of an appropriate type by asking the customer
+     *  what type of transaction is desired and then returning a newly-created
+     *  member of the appropriate subclass
+     *
+     *  @param atm the ATM used to communicate with customer
+     *  @param session the session in which this transaction is being performed
+     *  @param card the customer's card
+     *  @param pin the PIN entered by the customer
+     *  @return a newly created Transaction object of the appropriate type
+     *  @exception CustomerConsole.Cancelled if the customer presses cancel instead
+     *         of choosing a transaction type
+     */
+    /*->public static Transaction makeTransaction(ATM atm, Session session,
+                                              Card card, int pin)
+                                throws CustomerConsole.Cancelled   */
+
+    /** Peform a transaction.  This method depends on the three abstract methods
+     *  that follow to perform the operations unique to each type of transaction
+     *  in the appropriate way.
+     *
+     *  @return true if customer indicates a desire to do another transaction;
+     *          false if customer does not desire to do another transaction
+     *  @exception CardRetained if card was retained due to too many invalid PIN's
+     */
+    //->public boolean performTransaction() throws CardRetained
+
+    /** Perform the Invalid PIN Extension - reset session pin to new value if successful
+     *
+     *  @return status code returned by bank from most recent re-submission
+     *          of transaction
+     *  @exception CustomerConsole.Cancelled if customer presses the CANCEL key
+     *             instead of re-entering PIN
+     *  @exception CardRetained if card was retained due to too many invalid PIN's
+     */
+    //->public Status performInvalidPINExtension() throws CustomerConsole.Cancelled,
+                                                      //CardRetained*/
+
+    /** Get serial number of this transaction
+     *
+     *  @return serial number
+     */
+    int getSerialNumber();
+
+    /** Get specifics for the transaction from the customer - each
+     *  subclass must implement this appropriately.
+     *
+     *  @return message to bank for initiating this transaction
+     *  @exception CustomerConsole.Cancelled if customer cancelled this transaction
+     */
+    //->protected abstract Message getSpecificsFromCustomer() throws CustomerConsole.Cancelled;
+
+    /** Complete an approved transaction  - each subclass must implement
+     *  this appropriately.
+     *
+     *  @return receipt to be printed for this transaction
+     *  @exception CustomerConsole.Cancelled if customer cancelled this transaction
+     */
+    //->protected abstract Receipt completeTransaction() throws CustomerConsole.Cancelled;
 
 
+    // Local class representing card retained exception
 
 
+    /** Exception that is thrown when the customer's card is retained due to too
+     *  many invalid PIN entries
+     */
+    /*public static class CardRetained extends Exception
+    {
+        /** Constructor
+         */
+        /*public CardRetained()
+        {
+            super("Card retained due to too many invalid PINs");
+        }
+    }*/
+protected:
+    // Instance variables
+
+
+    /** ATM to use for communication with the customer
+     */
+    protected ATM atm;
+
+    /** Session in which this transaction is being performed
+     */
+    protected Session session;
+
+    /** Customer card for the session this transaction is part of
+     */
+    protected Card card;
+
+    /** PIN entered or re-entered by customer
+     */
+    protected int pin;
+
+    /** Serial number of this transaction
+     */
+    protected int serialNumber;
+
+    /** Message to bank describing this transaction
+     */
+    protected Message message;
+
+    /** Used to return account balances from the bank
+     */
+    protected Balances balances;
+
+private:
+    /** List of available transaction types to display as a menu
+     */
+    private static final String [] TRANSACTION_TYPES_MENU =
+        { "Withdrawal", "Deposit", "Transfer", "Balance Inquiry" };
+
+    /** Next serial number - used to assign a unique serial number to
+     *  each transaction
+     */
+    private static int nextSerialNumber = 1;
+
+    /** The current state of the transaction
+     */
+    private int state;
+
+    // Possible values for state
+
+    /** Getting specifics of the transaction from customer
+     */
+    private static final int GETTING_SPECIFICS_STATE = 1;
+
+    /** Sending transaction to bank
+     */
+    private static final int SENDING_TO_BANK_STATE = 2;
+
+    /** Performing invalid PIN extension
+     */
+    private static final int INVALID_PIN_STATE = 3;
+
+    /** Completing transaction
+     */
+    private static final int COMPLETING_TRANSACTION_STATE = 4;
+
+    /** Printing receipt
+     */
+    private static final int PRINTING_RECEIPT_STATE = 5;
+
+    /** Asking if customer wants to do another transaction
+     */
+    private static final int ASKING_DO_ANOTHER_STATE = 6;
+
+};
+}
+}
 #endif // TRANSACTION_H
+
