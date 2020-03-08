d96b96569171c69db1c05d77d398ad448cc657b6
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-08 18:13:20 +0100

    Update Withdrawal files

--- src/atm/transaction/Withdrawal.cpp
+++ src/atm/transaction/Withdrawal.cpp
@@ -1,7 +1,19 @@
-#ifndef WITHDRAWAL_H
-#define WITHDRAWAL_H
+#include <QDebug>
+#include "Withdrawal.h"
+
+atm::transaction::Withdrawal::Withdrawal()
+{
+
+}
+atm::transaction::Withdrawal::Withdrawal(ATM *ap_atm, Session *ap_session, Card *ap_card, int a_pin)
+{
+
+}
+
+atm::transaction::Withdrawal::~Withdrawal()
+{
+
+}
 
 
 
-
-#endif // WITHDRAWAL_H
--- src/atm/transaction/Withdrawal.h
+++ src/atm/transaction/Withdrawal.h
@@ -1,7 +1,67 @@
+/**
+ * @file Withdrawal.h
+ * @brief Header file of the Withdrawal class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
 #ifndef WITHDRAWAL_H
 #define WITHDRAWAL_H
+/** Representation for a cash withdrawal transaction
+ */
+class ATM;
+class Session;
+class Card;
+class Money;
+
+namespace atm
+{
+namespace transaction
+{
+class Withdrawal : public Transaction
+{
+
+public:
+    /** Default Constructor
+     */
+    Withdrawal();
+    /** Constructor
+     *
+     *  @param atm the ATM used to communicate with customer
+     *  @param session the session in which the transaction is being performed
+     *  @param card the customer's card
+     *  @param pin the PIN entered by the customer
+     */
+    Withdrawal(ATM *ap_atm, Session *ap_session, Card *ap_card, int a_pin);
+
+    /** Destructor
+     */
+    ~Withdrawal();
+    /** Get specifics for the transaction from the customer
+     *
+     *  @return message to bank for initiating this transaction
+     *  @exception CustomerConsole.Cancelled if customer cancelled this transaction
+     */
+    //->Message getSpecificsFromCustomer() throws CustomerConsole.Cancelled
+
+    /** Complete an approved transaction
+     *
+     *  @return receipt to be printed for this transaction
+     */
+    //->Receipt completeTransaction()
+protected:
 
 
+private:
+    /** Account to withdraw from
+     */
+    int m_from;
 
+    /** Amount of money to withdraw
+     */
+    Money m_amount;
 
+};
+}
+}
 #endif // WITHDRAWAL_H
