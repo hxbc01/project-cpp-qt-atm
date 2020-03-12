0c32358625eae9e8fa735de7479427b4c59984c2
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-12 19:09:25 +0100

    Update BillsPanel files

---
+++ src/simulation/BillsPanel.cpp
@@ -0,0 +1,16 @@
+#include <QDebug>
+#include "BillsPanel.h"
+
+simulation::BillsPanel::BillsPanel()
+{
+
+}
+
+simulation::BillsPanel::~BillsPanel()
+{
+
+}
+
+
+
+
---
+++ src/simulation/BillsPanel.h
@@ -0,0 +1,35 @@
+/**
+ * @file BillsPanel.h
+ * @brief Header file of the BillsPanel class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
+#ifndef BILLSPANEL_H
+#define BILLSPANEL_H
+/** Representation for customer's ATM card
+ */
+namespace simulation
+{
+class BillsPanel
+{
+
+public:
+    /** Default Constructor
+     */
+    BillsPanel();
+    /** Destructor
+     */
+    ~BillsPanel();
+
+protected:
+
+
+private:
+
+
+};
+}
+
+#endif // BILLSPANEL_H
+
