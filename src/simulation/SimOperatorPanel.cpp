5146c104f6b31b89ee990b51f789eac930626d2d
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-12 19:06:00 +0100

    Update SimOperatorPanel files

---
+++ src/simulation/SimOperatorPanel.cpp
@@ -0,0 +1,17 @@
+#include <QDebug>
+#include "SimOperatorPanel.h"
+
+simulation::SimOperatorPanel::SimOperatorPanel()
+{
+
+}
+
+
+simulation::SimOperatorPanel::~SimOperatorPanel()
+{
+
+}
+
+
+
+
---
+++ src/simulation/SimOperatorPanel.h
@@ -0,0 +1,36 @@
+/**
+ * @file SimOperatorPanel.h
+ * @brief Header file of the SimOperatorPanel class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
+#ifndef SIMOPERATORPANEL_H
+#define SIMOPERATORPANEL_H
+/** Simulate the operator panel
+ */
+namespace simulation
+{
+class SimOperatorPanel
+{
+
+public:
+    /** Default Constructor
+     */
+    SimOperatorPanel();
+    /** Destructor
+     */
+    ~SimOperatorPanel();
+
+
+protected:
+
+
+private:
+
+
+};
+}
+
+#endif // SIMOPERATORPANEL_H
+
