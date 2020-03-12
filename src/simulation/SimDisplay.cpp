5cd223d190d398c0272a2b05162db387e7bb92ec
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-12 19:02:47 +0100

    Update SimDisplay files

---
+++ src/simulation/SimDisplay.cpp
@@ -0,0 +1,16 @@
+#include <QDebug>
+#include "SimDisplay.h"
+
+simulation::SimDisplay::SimDisplay()
+{
+
+}
+
+
+simulation::SimDisplay::~SimDisplay()
+{
+
+}
+
+
+
---
+++ src/simulation/SimDisplay.h
@@ -0,0 +1,34 @@
+/**
+ * @file SimDisplay.h
+ * @brief Header file of the SimDisplay class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
+#ifndef SIMDISPLAY_H
+#define SIMDISPLAY_H
+/** Representation for customer's ATM card
+ */
+namespace simulation
+{
+class SimDisplay
+{
+
+public:
+    /** Default Constructor
+     */
+    SimDisplay();
+    /** Destructor
+     */
+    ~SimDisplay();
+
+protected:
+
+
+private:
+
+};
+}
+
+#endif // SIMDISPLAY_H
+
