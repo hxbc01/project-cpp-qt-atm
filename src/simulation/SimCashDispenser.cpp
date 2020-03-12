4792769e15bf6dee62f7a9932732ddc537f6bca0
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-12 19:14:03 +0100

    Update SimCashDispenser files

---
+++ src/simulation/SimCashDispenser.cpp
@@ -0,0 +1,16 @@
+#include <QDebug>
+#include "SimCashDispenser.h"
+
+simulation::SimCashDispenser::SimCashDispenser()
+{
+
+}
+
+simulation::SimCashDispenser::~SimCashDispenser()
+{
+
+}
+
+
+
+
---
+++ src/simulation/SimCashDispenser.h
@@ -0,0 +1,36 @@
+/**
+ * @file SimCashDispenser.h
+ * @brief Header file of the SimCashDispenser class
+ * @author Farid Oubbati
+ * @date 08-july-2019
+ * @version 1.0
+ */
+#ifndef SIMCASHDISPENSER_H
+#define SIMCASHDISPENSER_H
+/** Representation for customer's ATM card
+ */
+namespace simulation
+{
+class SimCashDispenser
+{
+
+public:
+    /** Default Constructor
+     */
+    SimCashDispenser();
+
+    /** Destructor
+     */
+    ~SimCashDispenser();
+
+protected:
+
+
+private:
+
+
+};
+}
+
+#endif // SIMCASHDISPENSER_H
+
