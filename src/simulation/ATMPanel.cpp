ce7840c7caf178d8b25721ebf7f57a60b9c556c1
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-16 19:22:57 +0100

    Update ATMPanel files

--- src/simulation/ATMPanel.cpp
+++ src/simulation/ATMPanel.cpp
@@ -1,8 +1,33 @@
 #include <QDebug>
 #include "ATMPanel.h"
+#include <QVBoxLayout>
+#include <QLabel>
+#include "SimOperatorPanel.h"
 
 simulation::ATMPanel::ATMPanel()
 {
+
+}
+
+simulation::ATMPanel::ATMPanel(const GUI *ap_gui,
+                               SimOperatorPanel *ap_operatorPanel,
+                               SimCardReader *ap_cardReader,
+                               SimDisplay *ap_display,
+                               SimKeyboard *ap_keyboard,
+                               SimCashDispenser *ap_cashDispenser,
+                               SimEnvelopeAcceptor *ap_envelopeAcceptor,
+                               SimReceiptPrinter *ap_receiptPrinter)
+{
+
+
+    // Label for the ATM GUI
+    mp_ATMLabel = new QLabel("ATM");
+    // create a the ATM GUI layout
+    mp_ATMLayout = new QVBoxLayout(this);
+    // Add the ATM label
+    mp_ATMLayout->addWidget(mp_ATMLabel);
+    // Add the operator panel
+    mp_ATMLayout->addWidget(ap_operatorPanel);
 
 }
 
--- src/simulation/ATMPanel.h
+++ src/simulation/ATMPanel.h
@@ -7,17 +7,54 @@
  */
 #ifndef ATMPANEL_H
 #define ATMPANEL_H
-/** Representation for customer's ATM card
+#include <QWidget>
+
+/** The GUI panel that simulates the ATM itself
  */
+QT_BEGIN_NAMESPACE
+class QVBoxLayout;
+class QLabel;
+QT_END_NAMESPACE
+
 namespace simulation
 {
-class ATMPanel
+class GUI;
+class SimOperatorPanel;
+class SimCardReader;
+class SimDisplay;
+class SimKeyboard;
+class SimCashDispenser;
+class SimEnvelopeAcceptor;
+class SimReceiptPrinter;
+}
+
+namespace simulation
+{
+class ATMPanel : public QWidget
 {
 
 public:
     /** Default Constructor
      */
     ATMPanel();
+    /** Constructor
+     *
+     *  @param gui the the overall GUI
+     *  @param operatorPanel the simulation of the operator panel
+     *  @param cardReader the simulation of the card reader
+     *  @param display the simulation of the display
+     *  @param keyboard the simulation of the keyboard
+     *  @param cashDispenser the simulation of the cash dispenser
+     *  @param receiptPrinter the simulation of the receipt printer
+     */
+    ATMPanel(const GUI *ap_gui,
+             SimOperatorPanel *ap_operatorPanel,
+             SimCardReader *ap_cardReader,
+             SimDisplay *ap_display,
+             SimKeyboard *ap_keyboard,
+             SimCashDispenser *ap_cashDispenser,
+             SimEnvelopeAcceptor *ap_envelopeAcceptor,
+             SimReceiptPrinter *ap_receiptPrinter);
     /** Destructor
      */
     ~ATMPanel();
@@ -26,6 +63,10 @@ protected:
 
 
 private:
+    // main layout for the ATM GUI Window
+    QVBoxLayout *mp_ATMLayout =nullptr;
+    // Top label for the ATM GUI Window
+    QLabel *mp_ATMLabel =nullptr;
 
 
 };
