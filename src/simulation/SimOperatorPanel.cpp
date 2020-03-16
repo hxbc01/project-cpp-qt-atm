c73ba2479453a2a6ff7466340d67547e78775051
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-16 19:20:32 +0100

    Update SimOperatorPanel files

--- src/simulation/SimOperatorPanel.cpp
+++ src/simulation/SimOperatorPanel.cpp
@@ -1,14 +1,52 @@
 #include <QDebug>
 #include "SimOperatorPanel.h"
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QPushButton>
+
 
 simulation::SimOperatorPanel::SimOperatorPanel()
 {
 
 }
 
+simulation::SimOperatorPanel::SimOperatorPanel(const Simulation *ap_simulation)
+{
+    mp_infoLabel = new QLabel("Turn ATM ON");
+    mp_buttonONOFF = new QPushButton("&ON");
+    mp_buttonONOFF->setCheckable(true);
+    mp_operatorLayout = new QVBoxLayout();
+    mp_operatorLayout->addWidget(mp_infoLabel);
+    mp_operatorLayout->addWidget(mp_buttonONOFF);
+
+    connect(mp_buttonONOFF,SIGNAL(clicked()),this,SLOT(pushATMButton()));
+
+    setStyleSheet("background-color:rgb(128,128,255);");
+    setLayout(mp_operatorLayout);
+
+
+
+}
 
 simulation::SimOperatorPanel::~SimOperatorPanel()
 {
+
+}
+
+void simulation::SimOperatorPanel::pushATMButton()
+{
+    if (mp_buttonONOFF->isChecked()){
+        mp_buttonONOFF->setText("OFF");
+        mp_infoLabel->setText("Turn ATM OFF");
+        //simulation.switchChanged(true);
+
+    } else {
+        mp_buttonONOFF->setText("ON");
+        mp_infoLabel->setText("Turn ATM ON");
+        //simulation.switchChanged(false);
+
+    }
+
 
 }
 
--- src/simulation/SimOperatorPanel.h
+++ src/simulation/SimOperatorPanel.h
@@ -7,23 +7,52 @@
  */
 #ifndef SIMOPERATORPANEL_H
 #define SIMOPERATORPANEL_H
+#include <QWidget>
 /** Simulate the operator panel
  */
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QVBoxLayout;
+class QPushButton;
+QT_END_NAMESPACE
+
 namespace simulation
 {
-class SimOperatorPanel
+class Simulation;
+}
+namespace simulation
 {
+class SimOperatorPanel : public QWidget
+{
+    Q_OBJECT
 
+private slots:
+    /**
+     * On/OFF button clicked
+     */
+    void pushATMButton();
 public:
     /** Default Constructor
      */
     SimOperatorPanel();
+    /** Constructor
+     *
+     *  @param simulation the overall simulation object
+     */
+    SimOperatorPanel(const Simulation *ap_simulation);
+
     /** Destructor
      */
     ~SimOperatorPanel();
 
 
 protected:
+    // Label On/Off Info
+    QLabel *mp_infoLabel=nullptr;
+    // Button On/Off
+    QPushButton *mp_buttonONOFF=nullptr;
+    // Layout for the operator panel
+    QVBoxLayout *mp_operatorLayout=nullptr;
 
 
 private:
