52a9592d0e862050da7cf3b80d7ea2bc42b0b258
Farid Oubbati <f.oubbati@yahoo.f>
2020-03-20 19:28:27 +0100

    Update SimDisplay files

--- src/simulation/SimDisplay.cpp
+++ src/simulation/SimDisplay.cpp
@@ -1,8 +1,38 @@
 #include <QDebug>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QPushButton>
 #include "SimDisplay.h"
 
 simulation::SimDisplay::SimDisplay()
 {
+    mp_buttonLeftVLayout = new QVBoxLayout();
+    mp_buttonRightVLayout = new QVBoxLayout();
+    mp_displayHLayout = new QHBoxLayout();
+    mp_display = new QLabel("Hello");
+    mp_display->setBackgroundRole(QPalette::Base);
+    mp_display->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    mp_display->setSizeIncrement(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    mp_display->setScaledContents(true);
+    mp_display->setFixedSize(ATM_DISPLAY_WIDTH,ATM_DISPLAY_HEIGHT);
+    for (int i=0;i<m_numLeftButtons;++i)
+    {
+        mp_leftButtons[i] = new QPushButton();
+        mp_buttonLeftVLayout->addWidget(mp_leftButtons[i]);
+    }
+    for (int i=0;i<m_numRightButtons;++i)
+    {
+        mp_rightButtons[i] = new QPushButton();
+        mp_buttonRightVLayout->addWidget(mp_rightButtons[i]);
+    }
+    mp_displayHLayout->addLayout(mp_buttonLeftVLayout);
+    mp_displayHLayout->addWidget(mp_display);
+    mp_displayHLayout->addLayout(mp_buttonRightVLayout);
+    setLayout(mp_displayHLayout);
+
+
 
 }
 
--- src/simulation/SimDisplay.h
+++ src/simulation/SimDisplay.h
@@ -7,11 +7,18 @@
  */
 #ifndef SIMDISPLAY_H
 #define SIMDISPLAY_H
-/** Representation for customer's ATM card
+#include <QWidget>
+/** Simulate the display portion of the customer console
  */
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QVBoxLayout;
+class QPushButton;
+class QHBoxLayout;
+QT_END_NAMESPACE
 namespace simulation
 {
-class SimDisplay
+class SimDisplay : public QWidget
 {
 
 public:
@@ -26,6 +33,24 @@ protected:
 
 
 private:
+    // vertical layout for the dislay left buttons
+    QVBoxLayout *mp_buttonLeftVLayout=nullptr;
+    // vertical layout for the dislay right buttons
+    QVBoxLayout *mp_buttonRightVLayout=nullptr;
+    // number of buttons
+    enum { m_numLeftButtons = 4, m_numRightButtons = 4};
+    // left buttons array
+    QPushButton *mp_leftButtons[m_numLeftButtons];
+    // right buttons array
+    QPushButton *mp_rightButtons[m_numRightButtons];
+    // horizontal layout for the display
+    QHBoxLayout *mp_displayHLayout=nullptr;
+    // main display screen
+    QLabel *mp_display=nullptr;
+    // The following are used only for setting the ATM GUI size
+    const int ATM_DISPLAY_WIDTH = 300;
+    const int ATM_DISPLAY_HEIGHT = 260;
+
 
 };
 }
