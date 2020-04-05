13ecd7f511f3f1f90c549226ab6d8aa28684c30d
Farid Oubbati <f.oubbati@yahoo.f>
2020-04-05 10:58:12 +0200

    update main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "ATMMainWindow.h"
 #include <thread>
 #include <QDebug>
-//#include <ATM.h>
+#include <ATM.h>
 #include <Simulation.h>
 #include <ThreadGuard.h>
 
@@ -10,21 +10,15 @@ int main(int argc, char *argv[])
 {
     QApplication ATMApp(argc, argv);
     // Create an ATM Object
-//    atm::ATM *p_myATM = new atm::ATM(42, "Gordon College", "First National Bank of Podunk",
-//                          "nullptr" /* We're not really talking to a bank! */);
+    atm::ATM myATM(42, "Gordon College", "First National Bank of Podunk",
+                   "nullptr" /* We're not really talking to a bank! */);
 
-    atm::ATM *p_myATM=nullptr;
-    simulation::Simulation *p_ATMSimulation = new simulation::Simulation(p_myATM);
+    simulation::Simulation *p_ATMSimulation = simulation::Simulation::getInstance(&myATM);
 
     ATMMainWindow ATMMain(p_ATMSimulation);
-
-    // Start the Thread that runs the ATM
-
-//    std::thread ATMThread(p_myATM);
-//    utils::ThreadGuard threadGuard(ATMThread);
-
-
     ATMMain.show();
+    myATM.startATMThread();
+
     //qDebug()<<ATMMain.size();
     return ATMApp.exec();
 }
