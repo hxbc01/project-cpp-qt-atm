78705c6916aca7e84cd2b0eada9ec6118c3a2217
Farid Oubbati <f.oubbati@yahoo.f>
2020-04-05 10:55:01 +0200

    update CardPanel files

--- src/simulation/CardPanel.cpp
+++ src/simulation/CardPanel.cpp
@@ -1,8 +1,30 @@
 #include <QDebug>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QIntValidator>
+#include <QString>
 #include "CardPanel.h"
 
 simulation::CardPanel::CardPanel()
 {
+    mp_infoLabel = new QLabel("Hallo");
+    mp_cardNumberField = new QLineEdit();
+    mp_cardNumberField->setPlaceholderText("Card number");
+    mp_cardNumberField->setFocus();
+    // validate input cash : between 0 dollar to 20*10000 dollars
+    mp_cardNumberField->setValidator(new QIntValidator(0,10000,mp_cardNumberField));
+
+    connect(mp_cardNumberField,SIGNAL(textEdited(QString)),this,SLOT(validateText()));
+
+    connect(mp_cardNumberField,SIGNAL(returnPressed()), &m_loop,SLOT(quit()));
+
+    mp_cardPanelVLayout = new QVBoxLayout();
+    mp_cardPanelVLayout->addWidget(mp_infoLabel);
+    mp_cardPanelVLayout->addWidget(mp_cardNumberField);
+    setLayout(mp_cardPanelVLayout);
+
+    m_cardNumber = 0;
 
 }
 
@@ -12,7 +34,26 @@ simulation::CardPanel::~CardPanel()
 
 }
 
+void simulation::CardPanel::validateText()
+{
+    if (!mp_cardNumberField->hasAcceptableInput()){
+        mp_cardNumberField->undo();
+        mp_cardNumberField->setText("Must be a valid integer >= 0");
+        mp_cardNumberField->selectAll();
+    }
+    else {
 
+        return;
+    }
+}
+
+int simulation::CardPanel::readCardNumber()
+{
+
+    m_cardNumber = mp_cardNumberField->text().toInt();
+    mp_cardNumberField->clear();
+    return m_cardNumber;
+}
 
 
 
--- src/simulation/CardPanel.h
+++ src/simulation/CardPanel.h
@@ -7,12 +7,32 @@
  */
 #ifndef CARDPANEL_H
 #define CARDPANEL_H
-/** Representation for customer's ATM card
+#include <QWidget>
+#include <mutex>
+#include <condition_variable>
+#include <QEventLoop>
+/** The GUI panel that simulates the reading of the ATM card's magnetic stripe
+ *  by asking the user to enter a Card number
  */
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QVBoxLayout;
+class QPushButton;
+class QHBoxLayout;
+class QLineEdit;
+QT_END_NAMESPACE
 namespace simulation
 {
-class CardPanel
+class CardPanel : public QWidget
 {
+    Q_OBJECT
+
+private slots:
+
+    /**
+     * validate entered number of bills
+     */
+    void validateText();
 
 public:
     /** Default Constructor
@@ -21,13 +41,28 @@ public:
     /** Destructor
      */
     ~CardPanel();
+    /** Ask the customer to enter the number on the card
+     *
+     *  @return the number entered
+     */
+    int readCardNumber();
+
+    // eventloop to wait until number of card entered
+    QEventLoop m_loop;
 
 
 protected:
 
 
 private:
-
+    // information about card number
+    QLabel *mp_infoLabel = nullptr;
+    // input card number
+    QLineEdit *mp_cardNumberField = nullptr;
+    // vertical layout for the Card panel
+    QVBoxLayout *mp_cardPanelVLayout = nullptr;
+    // Card number
+    int m_cardNumber;
 
 };
 }
