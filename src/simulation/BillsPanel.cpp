c7060ae564b870c4692adbcc28ddc88768158658
Farid Oubbati <f.oubbati@yahoo.f>
2020-04-05 10:55:52 +0200

    update BillsPanel files

--- src/simulation/BillsPanel.cpp
+++ src/simulation/BillsPanel.cpp
@@ -1,14 +1,63 @@
 #include <QDebug>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QIntValidator>
+
 #include "BillsPanel.h"
 
 simulation::BillsPanel::BillsPanel()
 {
+    // information text
+    mp_infoLabel = new QLabel("Hallo");
+
+    // enter number initial cash
+    mp_billsNumberField = new QLineEdit();
+    mp_billsNumberField->setPlaceholderText("Number of bills");
+    mp_billsNumberField->setFocus();
+    // validate input cash : between 0 dollar to 20*10000 dollars
+    mp_billsNumberField->setValidator(new QIntValidator(0,10000,mp_billsNumberField));
+
+
+    // connect clicked signal to slot
+    connect(mp_billsNumberField,SIGNAL(textEdited(QString)),this,SLOT(validateText()));
+    connect(mp_billsNumberField,SIGNAL(returnPressed()), &m_loop,SLOT(quit()));
+
+
+    mp_billsPanelVLayout = new QVBoxLayout();
+    mp_billsPanelVLayout->addWidget(mp_infoLabel);
+    mp_billsPanelVLayout->addWidget(mp_billsNumberField);
+    setLayout(mp_billsPanelVLayout);
+
+    m_billsNumber = 0;
+
 
 }
 
 simulation::BillsPanel::~BillsPanel()
 {
 
+}
+
+void simulation::BillsPanel::validateText()
+{
+    if (!mp_billsNumberField->hasAcceptableInput()){
+        mp_billsNumberField->undo();
+        mp_billsNumberField->setText("Must be a valid integer >= 0");
+        mp_billsNumberField->selectAll();
+    }
+    else {
+
+        return;
+    }
+}
+
+int simulation::BillsPanel::readBills()
+{
+    m_billsNumber = mp_billsNumberField->text().toInt();
+    mp_billsNumberField->clear();
+
+    return m_billsNumber;
 }
 
 
--- src/simulation/BillsPanel.h
+++ src/simulation/BillsPanel.h
@@ -7,12 +7,29 @@
  */
 #ifndef BILLSPANEL_H
 #define BILLSPANEL_H
-/** Representation for customer's ATM card
+#include <QWidget>
+#include <QEventLoop>
+/** The GUI panel that allows the operator to enter the number of bills in the
+ *  ATM at startup
  */
+QT_BEGIN_NAMESPACE
+class QLabel;
+class QVBoxLayout;
+class QPushButton;
+class QLineEdit;
+QT_END_NAMESPACE
 namespace simulation
 {
-class BillsPanel
+class BillsPanel : public QWidget
 {
+    Q_OBJECT
+
+private slots:
+
+    /**
+     * validate entered number of bills
+     */
+    void validateText();
 
 public:
     /** Default Constructor
@@ -21,11 +38,28 @@ public:
     /** Destructor
      */
     ~BillsPanel();
+    /** Ask the operator to enter the number of bills in the cash dispenser
+     *
+     *  @return the number entered
+     */
+    int readBills();
+
+    // eventloop to wait until number of bills entered
+    QEventLoop m_loop;
 
 protected:
 
 
 private:
+    // input bills number
+    QLineEdit *mp_billsNumberField = nullptr;
+    // information about bills
+    QLabel *mp_infoLabel = nullptr;
+    // vertical layout for the bills panel
+    QVBoxLayout *mp_billsPanelVLayout = nullptr;
+    // bills number
+    int m_billsNumber;
+
 
 
 };
