d7c0c2fa7486c32c073dcb8f3bdfb2a456c5e38e
Farid Oubbati <f.oubbati@yahoo.f>
2020-04-26 23:32:29 +0200

    Update SimKeyboard files

--- src/simulation/SimKeyboard.cpp
+++ src/simulation/SimKeyboard.cpp
@@ -6,6 +6,8 @@
 #include <QPushButton>
 simulation::SimKeyboard::SimKeyboard(SimDisplay *ap_display, SimEnvelopeAcceptor *ap_envelopeAcceptor)
 {
+    mp_display = ap_display;
+    mp_envelopeAcceptor = ap_envelopeAcceptor;
     mp_keyboardGridLayout = new QGridLayout();
     for (int i=0;i<m_buttons;++i)
     {
@@ -13,16 +15,270 @@ simulation::SimKeyboard::SimKeyboard(SimDisplay *ap_display, SimEnvelopeAcceptor
         int column = i % (m_buttons/4);
         int row = i / (m_buttons/4);
         mp_keyboardGridLayout->addWidget(mp_button[i], row, column);
+        // check if the pressed digit key is an integer {0,1,2,3,4,5,6,7,8,9}
+        bool l_isDigit;
+        //m_buttonsLabels.at(i).toInt(&l_isDigit);
+        mp_button[i]->text().toInt(&l_isDigit);
+        if (l_isDigit){
+
+            mp_button[i]->setShortcut((0x30 + mp_button[i]->text().toInt()));
+            mp_button[i]->setToolTip(mp_button[i]->text());
+
+            connect(mp_button[i], &QPushButton::clicked,
+                     [=]() { digitKeyPressed(m_buttonsLabels.at(i).toInt()); });
+        } else {
+            switch (i)
+            {
+                case 3: // Cancel Key
+                {
+                    mp_button[i]->setShortcut(Qt::Key_Escape);
+                    mp_button[i]->setToolTip(mp_button[i]->text());
+                    break;
+                }
+                case 7: // Clear Key
+                {
+                    mp_button[i]->setShortcut(Qt::Key_Delete);
+                    mp_button[i]->setToolTip(mp_button[i]->text());
+                    break;
+                }
+                case 11: // Enter Key
+                {
+                    mp_button[i]->setShortcut(Qt::Key_Enter);
+                    mp_button[i]->setToolTip(mp_button[i]->text());
+                    break;
+                }
+            }
+        }
     }
+
+    // Enter key
+    connect(mp_button[11],SIGNAL(clicked()), this,SLOT(enterKeyPressed()));
+
+    // quit event loop when Enter key clicked
+    //connect(mp_button[11],SIGNAL(clicked()), &m_eventLoop,SLOT(quit()));
+
+    // Cancel Key
+    connect(mp_button[3],SIGNAL(clicked()), this,SLOT(cancelKeyPressed()));
+
+    // Clear Key
+    connect(mp_button[7],SIGNAL(clicked()), this,SLOT(clearKeyPressed()));
+
+
     setLayout(mp_keyboardGridLayout);
+
+    m_mode = IDLE_MODE;
 
 }
 
 simulation::SimKeyboard::~SimKeyboard()
 {
+    if (m_eventLoop.isRunning()){
+        m_eventLoop.quit();
+    }
 
 }
 
 
 
+
+void simulation::SimKeyboard::setEcho(const QString &a_echo)
+{
+    mp_display->setEcho(a_echo);
+}
+
+QString* simulation::SimKeyboard::readInput(const int &a_mode, const int &a_maxValue)
+{
+    m_mode = a_mode;
+    m_maxValue = a_maxValue;
+    m_currentInput.clear();
+    m_cancelled = false;
+    if (m_mode == AMOUNT_MODE)
+    {
+        setEcho("0.00");
+    }
+    else
+    {
+        setEcho("");
+    }   
+
+    // set focus to keyboard
+    setFocus();
+
+    // wait()
+    m_eventLoop.exec();
+    // ------
+
+    m_mode = IDLE_MODE;
+    qDebug()<< "m_currentInput = "<<m_currentInput;
+    qDebug()<< "m_cancelled = "<<m_cancelled;
+    qDebug()<< "IDLE_MODE";
+
+
+    if (m_cancelled)
+        return nullptr;
+    else
+        return &m_currentInput;
+
+
+}
+
+void simulation::SimKeyboard::digitKeyPressed(int digit)
+{
+
+    switch (m_mode)
+    {
+        case IDLE_MODE:
+        {
+            qDebug()<< "IDLE_MODE";
+            break;
+        }
+        case PIN_MODE:
+        {
+            qDebug()<< "digitKeyPressed";
+            m_currentInput.append(QString::number(digit));
+            setEcho(QString("*"));
+            break;
+        }
+
+//        case AMOUNT_MODE:
+//        {
+//            m_currentInput.append(digit);
+//            QString l_input = m_currentInput;
+//            if (l_input.length() == 1)
+//                setEcho("0.0" + l_input);
+//            else if (l_input.length() == 2)
+//                setEcho("0." + l_input);
+//            else
+//                setEcho(input.substring(0, input.length() - 2) + "." +
+//                    input.substring(input.length() - 2));
+//            break;
+//        }
+
+        case MENU_MODE:
+        {
+            if (digit > 0 && digit <= m_maxValue)
+            {
+                m_currentInput.append(QString::number(digit));
+            }
+            else {
+                qDebug()<< "beep";
+            }
+            break;
+        }
+    }
+
+}
+
+void simulation::SimKeyboard::enterKeyPressed()
+{
+    switch(m_mode)
+    {
+        case IDLE_MODE:
+        {
+            break;
+        }
+        case PIN_MODE:
+        {
+            if (m_currentInput.length() > 0){
+                m_eventLoop.quit();
+            }
+            else {
+                qDebug()<< "beep";
+            }
+            break;
+        }
+        case AMOUNT_MODE:
+        {
+            if (m_currentInput.length() > 0){
+
+                m_eventLoop.quit();
+            }
+            else {
+                qDebug()<< "beep";
+            }
+            break;
+        }
+        case MENU_MODE:
+        {
+            qDebug()<< "beep";
+            break;
+        }
+    }
+
+}
+
+void simulation::SimKeyboard::clearKeyPressed()
+{
+    switch(m_mode)
+    {
+        case IDLE_MODE:
+        {
+            break;
+        }
+        case PIN_MODE:
+        {
+            m_currentInput.clear();
+            setEcho("");
+            break;
+        }
+        case AMOUNT_MODE:
+        {
+
+            m_currentInput.clear();
+            setEcho("0.00");
+            break;
+        }
+        case MENU_MODE:
+        {
+            qDebug()<< "beep";
+            break;
+        }
+    }
+
+}
+
+void simulation::SimKeyboard::cancelKeyPressed()
+{
+    switch(m_mode)
+    {
+        case IDLE_MODE:
+        {
+            // It is possible to press the cancel key when requested
+            // to insert an envelope - so notify the envelope acceptor
+            // of this fact (notification is ignored if acceptor is
+            // not waiting for an envelope)
+
+//            synchronized(envelopeAcceptor)
+//            {
+//                envelopeAcceptor.notify();
+//            }
+            m_cancelled = true;
+            m_eventLoop.quit();
+            break;
+
+        }
+        case PIN_MODE:
+        {
+            m_cancelled = true;
+            m_eventLoop.quit();
+            qDebug()<< "Cancel PIN_MODE";
+
+            break;
+        }
+        case AMOUNT_MODE:
+        {
+            m_cancelled = true;
+            m_eventLoop.quit();
+            break;
+        }
+        case MENU_MODE:
+        {
+            m_cancelled = true;
+            m_eventLoop.quit();
+            break;
+        }
+
+    }
+
+}
 
--- src/simulation/SimKeyboard.h
+++ src/simulation/SimKeyboard.h
@@ -8,6 +8,8 @@
 #ifndef SIMKEYBOARD_H
 #define SIMKEYBOARD_H
 #include <QWidget>
+#include <QEventLoop>
+#include "Simulation.h"
 /** Simulate the keyboard portion of the customer console
  */
 QT_BEGIN_NAMESPACE
@@ -23,6 +25,25 @@ namespace simulation
 {
 class SimKeyboard : public QWidget
 {
+    Q_OBJECT
+
+private slots:
+
+    /** Handle a digit key
+      *
+      *  @param digit the value on the key
+      */
+    void digitKeyPressed(int digit);
+    /** Handle the ENTER key
+     */
+    void enterKeyPressed();
+    /** Handle the CLEAR key
+     */
+    void clearKeyPressed();
+    /** Handle the CANCEL KEY
+      */
+    void cancelKeyPressed();
+
 
 public:
     /** Default Constructor
@@ -31,11 +52,27 @@ public:
     /** Destructor
      */
     ~SimKeyboard();
+    /** Read input from the keyboard
+      *
+      *  @param mode the input mode to use - one of the constants defined below.
+      *  @param maxValue the maximum acceptable value (used in MENU_MODE only)
+      *  @return the line that was entered - null if user pressed CANCEL.
+      */
+     QString* readInput(const int &a_mode, const int &a_maxValue);
 
+     // eventloop to wait until PIN number of card entered
+     QEventLoop m_eventLoop;
 protected:
 
 
 private:
+
+    /** Set the echo string displayed on the display
+    *
+    *  @param echo the text to set the echo to (the whole line)
+    */
+    void setEcho(const QString &echo);
+
     // grid layout for the keyboard
     QGridLayout *mp_keyboardGridLayout=nullptr;
     // number of buttons
@@ -44,6 +81,50 @@ private:
     QPushButton *mp_button[m_buttons];
     // left buttons labels
     QStringList m_buttonsLabels = {"1", "2", "3","CANCEL", "4", "5", "6", "CLEAR","7", "8", "9", "ENTER", "", "0", "", ""};
+
+    /** The display onto which to echo input
+     */
+    SimDisplay *mp_display=nullptr;
+
+    /** The envelope acceptor to notify if CANCEL is pressed while not awaiting
+     *  customer input
+     */
+    SimEnvelopeAcceptor *mp_envelopeAcceptor=nullptr;
+
+    /** Current input mode - one of the values defined below
+     */
+    int m_mode;
+
+    /** Not currently reading input - ignore keys (except CANCEL)
+     */
+    static const int IDLE_MODE = 0;
+
+    /** Read input in PIN mode - allow user to enter several characters,
+     *  and to clear the line if the user wishes; echo as asterisks
+     */
+    static const int PIN_MODE = Simulation::PIN_MODE;
+
+    /** Read input in amount mode - allow user to enter several characters,
+     *  and to clear the line if the user wishes; echo what use types
+     */
+    static const int AMOUNT_MODE = Simulation::AMOUNT_MODE;
+
+    /** Read input in menu choice mode - wait for one digit key to be pressed,
+     *  and return value immediately.
+     */
+    static const int MENU_MODE = Simulation::MENU_MODE;
+
+    /** Current partial line of input
+     */
+    QString m_currentInput;
+
+    /** Cancellation flag - set to true if user cancels
+     */
+    bool m_cancelled;
+
+    /** Maximum valid value - used in MENU_MODE only
+     */
+    int m_maxValue;
 
 
 };
